46d74c5667176b15bbb50c5e8524b810b6ad103e
Timo <tj.timo.j@gmail.com>
2017-06-26 09:22:32 +0200

    .

--- admin/model/extension/module/wuunder.php
+++ admin/model/extension/module/wuunder.php
@@ -7,15 +7,15 @@ class ModelExtensionModuleWuunder extends Model
     {
         $this->db->query("DROP TABLE IF EXISTS `wuunder_shipment`;");
         $this->db->query("CREATE TABLE IF NOT EXISTS wuunder_shipment (
-	  `shipment_id` int(10) NOT NULL AUTO_INCREMENT,
-	  `order_id` int(10) NOT NULL,
-	  `label_id` varchar(255) DEFAULT NULL,
-	  `label_url` text DEFAULT NULL,
-	  `label_tt_url` text DEFAULT NULL,
-	  `booking_token` varchar(255) NOT NULL,
-	  `booking_url` text DEFAULT NULL,
-	  PRIMARY KEY (`shipment_id`)
-	) ENGINE=InnoDB  DEFAULT CHARSET=utf8;");
+          `shipment_id` int(10) NOT NULL AUTO_INCREMENT,
+          `order_id` int(10) NOT NULL,
+          `label_id` varchar(255) DEFAULT NULL,
+          `label_url` text DEFAULT NULL,
+          `label_tt_url` text DEFAULT NULL,
+          `booking_token` varchar(255) NOT NULL,
+          `booking_url` text DEFAULT NULL,
+          PRIMARY KEY (`shipment_id`)
+        ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;");
     }
 
     public function uninstallTable()
@@ -50,8 +50,9 @@ class ModelExtensionModuleWuunder extends Model
         $this->db->query("UPDATE `wuunder_shipment` SET booking_url = '" . $booking_url . "' WHERE order_id = " . $order_id . " AND booking_token = '" . $booking_token . "'");
     }
 
-    public function getOrderStatusCode($order_id) {
-        $query = $this->db->query("SELECT order_status_id FROM `" . DB_PREFIX . "order` o WHERE o.order_id = ". $order_id);
+    public function getOrderStatusCode($order_id)
+    {
+        $query = $this->db->query("SELECT order_status_id FROM `" . DB_PREFIX . "order` o WHERE o.order_id = " . $order_id);
         if ($query->num_rows) {
             return $query->row['order_status_id'];
         } else {
